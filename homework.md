b26b9af08768d427e842debbfbdf39a6a03fc5ad
tarunsaha2424 <tarunsaha2424@gmail.com>
2026-01-11 16:40:29 +0530

    schema validations

--- homework.md
+++ homework.md
@@ -49,4 +49,18 @@
 - User.findOne with duplicate email ids, which object will be returned
 - API- Get user by email
 -  API - Feed API - GET/feed - get all the users from the database
+- API - Get user by ID
+- Create a delete user API
+- Difference between PATCH and PUT
+- API -Update a user
+- Explore the Mongoose Documentation for Model methods
+- What are options in a Model.findOneAndUpdate method, explore more about it
+- API - Update the user with email ID
 
+- Explore schema type options from the documentation
+- add required, unique, lowercase , min, minLength, trim
+- Add default
+- Create a custom validate function for gender
+- Improve the DB schema
+- Improve the DB schema - PUT all appropriate validation on each field in Schema
+- Add timestamps to the userSchema
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,54 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   firstName: {
-    type:String
+    type:String,
+    required: true,
+    minLength: 4,
+    maxLength: 50,   
   },
   lastName:{
     type: String
   },
   emailId:{
-    type: String
+    type: String,
+    lowercase:true,
+    required: true,
+    unique:true,
+    trim:true,  
   },
-  password:{
-    type: String
+  password:{ 
+    type: String,
+    required: true,
   },
   age:{
-    type: Number
+    type: Number,
+    min: 18 ,
+   
   }, 
   gender:{
-    type: String
-  }
+    type: String,
+    validate(value){
+      if(!["male","female","others"].includes(value)){
+        throw new Error("Gender data is not valid");
+      }
+    }
+  },
+    photoUrl:{
+      type: String,
+      default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzPigd8zbsOzKSxjjC1yN2-C7036FoJxrp1Q&s"
+    } ,
+    about:{
+      type: String,
+      default: "This is a default about of the user!!"
+    },
+    skills:{
+      type: [String]
+    },
+    createdAt:{
+      type: Date
+    }
+},{
+  timestamps: true,
 })
 
 
